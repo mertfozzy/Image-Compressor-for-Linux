f013dd91fee608af3e9e9d00637258cc6e671d48
Mert Altuntaş <mertfozzy@gmail.com>
2022-01-05 18:09:40 +0300

    HUGE UPDATE WITHOUT LECTURE

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QFrame
+from PyQt5.QtWidgets import QApplication, QFileDialog,QInputDialog, QWidget, QMainWindow, QFrame, QLabel, QLineEdit, QPushButton, QComboBox
 from PyQt5.QtGui import QIcon
+from PIL import Image
+from PyQt5.QtCore import Qt
+import os
+import PIL
+import time
+import sys, os
 
 class App(QMainWindow):
 
@@ -33,11 +39,34 @@ class App(QMainWindow):
         self.single_bubble.setObjectName("bubble") # connect with qss
         self.single_bubble.move(50, 100) # locate
         self.single_bubble.mousePressEvent = self.single_bubble_clicked # click event
+        
+        self.single_bubble_heading = QLabel(self.single_bubble)
+        self.single_bubble_heading.setObjectName("bubble_heading")
+        self.single_bubble_heading.setText("Compress Image")
+        self.single_bubble_heading.move(90, 8)
+
+        self.single_bubble_para = QLabel(self.single_bubble)
+        self.single_bubble_para.setObjectName("bubble_para")
+        self.single_bubble_para.setText("Click here to compress single image!")
+        self.single_bubble_para.move(25, 32)
+
+
 
         self.dir_bubble = QFrame(self) # create 2. frame named dir_bubble
         self.dir_bubble.setObjectName("bubble") # connect with qss
         self.dir_bubble.move(50, 275) # locate
         self.dir_bubble.mousePressEvent = self.dir_bubble_clicked # click event
+
+        self.dir_bubble_heading = QLabel(self.dir_bubble)
+        self.dir_bubble_heading.setObjectName("bubble_heading")
+        self.dir_bubble_heading.setText("Compress Multiple Images")
+        self.dir_bubble_heading.move(55, 8)
+
+        self.dir_bubble_para = QLabel(self.dir_bubble)
+        self.dir_bubble_para.setText("Want to compress multiple images at once? select the folder and get compressed version of the images in another folder.")
+        self.dir_bubble_para.setWordWrap(True)
+        self.dir_bubble_para.setObjectName("bubble_para")
+        self.dir_bubble_para.move(10, 32)
 
         #=============================================
 
